f1ef741bc8cf134c5d3b2f4337ec7085197d6ccf
Mónus Bence <monusbence3@gmail.com>
2023-05-24 19:54:12 +0200

    Nagyjából kész a feladat!

---
+++ Hf 2023.05.24/Operatorok.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hf_2023._05._24_
+{
+    internal class Operatorok
+    {
+        int elsoSzam;
+        string muveletiJel;
+        int masikSzam;
+
+        public Operatorok(string[] adatok)
+        {
+            this.elsoSzam = Convert.ToInt32(adatok[0]);
+            this.muveletiJel = adatok[1];
+            this.masikSzam = Convert.ToInt32(adatok[2]);
+        }
+
+        public int ElsoSzam { get => elsoSzam; }
+        public string MuveletiJel { get => muveletiJel; }
+        public int MasikSzam { get => masikSzam; }
+
+        public string HatosFeladat() {
+            string[] operatorok = { "+", "-", "*", "/", "mod", "div" };
+            if (operatorok.Contains(muveletiJel))
+            {
+                return "Hibás operátor!";
+            }
+            if ((muveletiJel == "mod" || muveletiJel == "div" || muveletiJel == "/") && (elsoSzam == 0 || masikSzam == 0))
+            {
+                return "Egyéb hiba!";
+            }
+
+            switch (muveletiJel)
+            {
+                case "div":
+                    return $"{elsoSzam} {muveletiJel} {masikSzam} = {elsoSzam / masikSzam}" ;
+                case "mod":
+                    return $"{elsoSzam} {muveletiJel} {masikSzam} = {elsoSzam % masikSzam}";
+                case "/":
+                    return $"{elsoSzam} {muveletiJel} {masikSzam} = {Convert.ToDouble(elsoSzam) / masikSzam}";
+                case "-":
+                   return $"{elsoSzam} {muveletiJel} {masikSzam} = {elsoSzam - masikSzam}";
+                case "+":
+                    return $"{elsoSzam} {muveletiJel} {masikSzam} = {elsoSzam + masikSzam}";
+                case "*":
+                    return $"{elsoSzam} {muveletiJel} {masikSzam} = {elsoSzam * masikSzam}";
+                default:
+                    return "";
+            }
+
+        }
+    }
+}
--- Hf 2023.05.24/Program.cs
+++ Hf 2023.05.24/Program.cs
@@ -4,6 +4,31 @@
     {
         static void Main(string[] args)
         {
+            List <Operatorok> lista = File.ReadAllLines("../../../kifejezesek.txt").Select(x => new Operatorok(x.Split())).ToList();
+
+            Console.WriteLine($"2. feladat: Kifejezések száma: {lista.Count}");
+            Console.WriteLine($"3. feladat: Kifejezések maradékos osztással: {lista.Count(x => x.MuveletiJel == "mod")}");
+            Console.WriteLine($"4. feladat: {(lista.Any(x => x.ElsoSzam % 10 == 0 && x.MasikSzam % 10 == 0)? "Van ilyen kifejezés!" : "Nincs ilyen kifejezés! :(")}");
+            Console.WriteLine("5. feladat: Statisztika:\n" +
+                    $"\tmod -> {lista.Count(x => x.MuveletiJel == "mod")} db\n" +
+                    $"\t/ -> {lista.Count(x => x.MuveletiJel == "/")} db\n" +
+                    $"\tdiv -> {lista.Count(x => x.MuveletiJel == "div")} db\n" +
+                    $"\t- -> {lista.Count(x => x.MuveletiJel == "-")} db\n" +
+                    $"\t* -> {lista.Count(x => x.MuveletiJel == "*")} db\n" +
+                    $"\t+ -> {lista.Count(x => x.MuveletiJel == "+")} db");
+            Console.WriteLine("7. feladat: Kérek egy kifejezést (pl.: 1 + 1): ");
+            string bekert = Console.ReadLine();
+            while (bekert != "vége") 
+            {
+                Operatorok muvelet = new Operatorok(bekert.Split());
+                Console.WriteLine(muvelet.HatosFeladat());
+                Console.WriteLine("7. feladat: Kérek egy kifejezést (pl.: 1 + 1): ");
+                bekert = Console.ReadLine();    
+                
+
+            }
+            Console.WriteLine("8. feladat: eredmenyek.txt");
+            File.WriteAllLines("../../../eredmenyek.txt", lista.Select(x => x.HatosFeladat()));
 
         }
     }
